27d03325d25de1bdd42b19332cc94ba21673e130
cbcook <cbcook321@gmail.com>
2017-04-05 09:05:28 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 var app = express();
-var port =  8080,
-    ip   =  '0.0.0.0';
+var port = 8080;
+	ip = '0.0.0.0';
 
 app.use(express.static(__dirname + "/assets"));
 
 app.listen(port, ip, function(){
-  console.log('server started at ' +  port);
+	console.log('server started at ' + port);
 });
